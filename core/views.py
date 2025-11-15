7dd7ba1e672f13294e8cabcc9cef99e5a1a9f3af
LRP2004 <lsruizpena@gmail.com>
2025-11-15 02:26:20 -0400

    Implement aside and new layout

--- core/views.py
+++ core/views.py
@@ -4,8 +4,4 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    messages.success(request, "Hola")
-    messages.info(request, "Hola")
-    messages.warning(request, "Hola")
-    messages.error(request, "Hola")
     return render(request, "index.html")
