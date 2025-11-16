1ff711fa837d6aa2f41400885c45e44f29fc2350
LRP2004 <lsruizpena@gmail.com>
2025-11-15 23:49:02 -0400

    Add last items to the aside and profile layout

--- base/urls.py
+++ base/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("core.urls")),
+    path("", include("users.urls")),
 ]
 urlpatterns += staticfiles_urlpatterns()
 
